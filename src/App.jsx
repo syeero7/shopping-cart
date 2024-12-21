b9713080f39a6a3bfeab6fdcdf69f37c402d87cd
syeero7 <EmalHasitha@proton.me>
2024-12-22 03:03:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import routes from "./routes";
+
+const App = () => {
+  return <>{routes}</>;
+};
+
+export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
+const routes = (
+  <Routes>
+    <Route path="/"></Route>
+  </Routes>
+);
+
+export default routes;
