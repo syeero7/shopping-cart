d8e9dffebe569dac888884404cda1689ee2489d9
syeero7 <EmalHasitha@proton.me>
2024-12-23 03:52:22 +0530

    feat: add Product component

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,66 @@
+import PropTypes from "prop-types";
+import addToCart from "../../assets/add-cart.svg";
+import { useState } from "react";
+
+function Product({ id, title, price, image, setCartItems }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setCartItems((c) => {
+      return [...c.filter((product) => product.id !== id), { id, quantity }];
+    });
+  };
+
+  return (
+    <article data-id={id}>
+      <h2>{title}</h2>
+      <div aria-hidden="true">
+        <img src={image} alt="" />
+      </div>
+      <strong>${price}</strong>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <button
+            type="button"
+            onClick={() => setQuantity((q) => q - 1)}
+            disabled={quantity === 1}
+          >
+            -
+          </button>
+          <input
+            aria-label="quantity"
+            type="number"
+            min="1"
+            name="quantity"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+          <button type="button" onClick={() => setQuantity((q) => q + 1)}>
+            +
+          </button>
+        </div>
+        <button type="submit" aria-label="add to cart">
+          <img
+            aria-hidden="true"
+            src={addToCart}
+            alt=""
+            width="50"
+            height="50"
+          />
+        </button>
+      </form>
+    </article>
+  );
+}
+
+Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  setCartItems: PropTypes.func.isRequired,
+};
+
+export default Product;
