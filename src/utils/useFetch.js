35494fafb78548df27e1b51e483ab44e4cf3388b
syeero7 <EmalHasitha@proton.me>
2024-12-23 03:33:01 +0530

    feat: add useFetch custom hook

---
+++ src/utils/useFetch.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok)
+          throw new Error(`${response.status}|${response.statusText}`);
+
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { data, error, loading };
+};
