f2ba19cf0bbfdff942c01e6ffc3098f686f0406e
syeero7 <EmalHasitha@proton.me>
2024-12-23 03:50:24 +0530

    test: add tests for the Product component

---
+++ tests/Product.test.jsx
@@ -0,0 +1,42 @@
+import { expect, it, describe, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Product from "../src/components/Product/Product";
+
+describe("Product", () => {
+  const PROPS = {
+    id: 1,
+    title: "shirt",
+    price: 10,
+    image: "image-url",
+    setCartItems: vi.fn(),
+  };
+
+  it("should render product component with given props", () => {
+    render(<Product {...PROPS} />);
+
+    const heading = screen.getByRole("heading");
+    const price = screen.getByRole("strong");
+
+    expect(heading).toHaveTextContent(PROPS.title);
+    expect(price).toHaveTextContent(`$${PROPS.price}`);
+  });
+
+  it("should increment/decrement the quantity when the button is clicked", async () => {
+    const DEFAULT_QUANTITY = 1;
+    const user = userEvent.setup();
+
+    render(<Product {...PROPS} />);
+
+    const incrementBtn = screen.getByRole("button", { name: "+" });
+    const decrementBtn = screen.getByRole("button", { name: "-" });
+    const input = screen.getByRole("spinbutton");
+
+    expect(decrementBtn).toBeDisabled();
+    await user.click(incrementBtn);
+    expect(parseInt(input.value)).toBeGreaterThan(DEFAULT_QUANTITY);
+    expect(decrementBtn).toBeEnabled();
+    await user.click(decrementBtn);
+    expect(parseInt(input.value)).toBe(DEFAULT_QUANTITY);
+  });
+});
