f506bd30a32f7fd64c3d6ef807805283ce83567e
syeero7 <EmalHasitha@proton.me>
2024-12-23 06:02:06 +0530

    feat: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
 import "./index.css";
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { createRoutesFromElements, Route } from "react-router-dom";
+import App from "./App";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Cart from "./pages/Cart/Cart";
 
-const routes = (
-  <Routes>
-    <Route path="/"></Route>
-  </Routes>
+const routes = createRoutesFromElements(
+  <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+    <Route errorElement={<ErrorPage />} />
+    <Route index element={<Home />} />
+    <Route path="products" element={<Products />} />
+    <Route path="cart" element={<Cart />} />
+  </Route>
 );
 
 export default routes;
