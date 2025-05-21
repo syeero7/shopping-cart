a04d2ec919dccb6830b771144e50cb3806f2d637
Emal <EmalHasitha@proton.me>
2025-05-21 19:40:58 +0530

    feat: style Navbar & Home components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,22 +7,32 @@ function Navbar({ itemCount }) {
       <nav className={styles.navbar}>
         <ul>
           <li>
-            <NavLink to="/">
+            <NavLink to="/" viewTransition>
               <span className="visually-hidden">home</span>
-              home svg
+
+              <span className={styles.logo}>
+                FAKE
+                <span>store</span>
+              </span>
             </NavLink>
           </li>
 
           <li>
-            <NavLink to="/products">Products</NavLink>
+            <NavLink to="/products" viewTransition>
+              Products
+            </NavLink>
           </li>
 
           <li className={styles.cartLink}>
-            <NavLink to="/cart">
+            <NavLink to="/cart" viewTransition>
               <span className="visually-hidden">shopping cart</span>
-              cart svg
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
+              </svg>
             </NavLink>
-            <span aria-label="cart item count">{itemCount}</span>
+            <span className={styles.count} aria-label="cart item count">
+              {itemCount}
+            </span>
           </li>
         </ul>
       </nav>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,16 @@
+import styles from "./Home.module.css";
+
 function Home() {
   return (
-    <>
-      <h1>Store</h1>
-    </>
+    <div className={styles.container}>
+      <h1>Fake store</h1>
+      <p>
+        Welcome to The Fake Store, your new favorite destination for all your
+        shopping needs! We're thrilled to have you here, whether you're looking
+        for everyday essentials, unique finds, or just want to browse and
+        discover something new.
+      </p>
+    </div>
   );
 }
 
