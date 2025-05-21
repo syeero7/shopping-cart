1f5932e295deb388f6e220c2e1958f9921bc1b23
Emal <EmalHasitha@proton.me>
2025-05-22 04:31:04 +0530

    feat: add checkout functionality

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,14 +1,17 @@
 import styles from "./CartItem.module.css";
 
-function CartItem({ id, title, price, image, quantity }) {
+function CartItem({ title, price, image, quantity, onClick }) {
   return (
-    <article data-id={id} className={styles.card}>
+    <article className={styles.card}>
       <h2>{title}</h2>
       <div aria-hidden="true" className={styles.img}>
         <img src={image} alt="" width="300" height="300" />
       </div>
       <strong>${price}</strong>
       <p>quantity: {quantity}</p>
+      <div>
+        <button onClick={onClick}>Remove from cart</button>
+      </div>
     </article>
   );
 }
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,9 +3,24 @@ import CartItem from "../../components/CartItem/CartItem";
 import styles from "./Cart.module.css";
 
 function Cart() {
-  const { data, cartItems } = useOutletContext();
+  const { data, cartItems, setCartItems } = useOutletContext();
 
   const items = data?.filter((item) => cartItems.has(item.id));
+  const total =
+    items?.reduce(
+      (count, item) => count + item.price * cartItems.get(item.id),
+      0
+    ) || 0;
+
+  const removeItem = (id) => {
+    return () => {
+      setCartItems((prev) => {
+        const map = new Map(prev);
+        map.delete(id);
+        return map;
+      });
+    };
+  };
 
   return (
     <>
@@ -15,21 +30,28 @@ function Cart() {
           <p>cart is empty</p>
         </div>
       ) : (
-        <article className={styles.container}>
-          <ul>
-            {items.map((item) => (
-              <li key={item.id}>
-                <CartItem
-                  id={item.id}
-                  title={item.title}
-                  price={item.price}
-                  quantity={cartItems.get(item.id)}
-                  image={item.image}
-                />
-              </li>
-            ))}
-          </ul>
-        </article>
+        <>
+          <article className={styles.container}>
+            <ul>
+              {items.map((item) => (
+                <li key={item.id}>
+                  <CartItem
+                    title={item.title}
+                    price={item.price}
+                    quantity={cartItems.get(item.id)}
+                    image={item.image}
+                    onClick={removeItem(item.id)}
+                  />
+                </li>
+              ))}
+            </ul>
+          </article>
+          <hr />
+          <div className={styles.checkout}>
+            <strong>Total ${total.toFixed(2)}</strong>
+            <button onClick={() => setCartItems(new Map())}>Checkout</button>
+          </div>
+        </>
       )}
     </>
   );
